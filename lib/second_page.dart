16a9c7404459dcf1b8710ce49fee84cb020e6829
Daniel Felten <mail@dfelten.de>
2020-01-21 14:55:48 +0100

    Use ListView builder

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -4,22 +4,29 @@ import 'package:flutter/material.dart';
 class SecondPage extends StatelessWidget {
   static const routeName = '/second';
 
+  List<Item> items = [
+    Item(text: 'Map', icon: Icons.map),
+    Item(text: 'Event', icon: Icons.event),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Second Page")),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
-          ),
-          ListTile(
-            leading: Icon(Icons.event),
-            title: Text('Event'),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: Icon(items[index].icon),
+          title: Text(items[index].text),
+        ),
       ),
     );
   }
+}
+
+class Item {
+  Item({this.text, this.icon});
+
+  final String text;
+  final IconData icon;
 }
