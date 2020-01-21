41147823cae91ac85b45a2cecda05c5c3cc52322
Daniel Felten <mail@dfelten.de>
2020-01-21 16:06:24 +0100

    Generate model

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,11 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'todo.g.dart';
+
+@JsonSerializable()
 class Todo {
   Todo({this.userId, this.id, this.title, this.completed});
 
-  Todo.fromJson(Map<String, dynamic> json)
-      : userId = json['userId'],
-        id = json['id'],
-        title = json['title'],
-        completed = json['completed'];
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
 
   final int userId;
   final int id;
